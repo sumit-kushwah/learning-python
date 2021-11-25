dfefe0a30b43d4a4aab523434808a6639a43622b
sumit Kushwah <sumitkushwah@sumits-MacBook-Air.local>
2021-11-25 14:52:26 +0530

    course finished

--- files/Ch5/htmlparsing_start.py
+++ files/Ch5/htmlparsing_start.py
@@ -1,13 +1,24 @@
-# 
+#
 # Example file for parsing and processing HTML
 #
+from html.parser import HTMLParser
+
+metacount = 0
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global metacount
+        if tag == "meta":
+            metacount += 1
+
+        print('encounterd a start tag:', tag)
 
 
 def main():
-  # instantiate the parser and feed it some HTML
-  parser = MyHTMLParser()
-    
+    # instantiate the parser and feed it some HTML
+    parser = MyHTMLParser()
+
 
 if __name__ == "__main__":
-  main();
-  
+    main()
--- files/Ch5/inetdata_start.py
+++ files/Ch5/inetdata_start.py
@@ -1,6 +1,15 @@
-# 
+#
 # Example file for retrieving data from the internet
 #
 
+import urllib.request
+
+
+def main():
+    webUrl = 'https://www.google.com'
+    result = urllib.request.urlopen(webUrl)
+    print(result.read())
+
+
 if __name__ == "__main__":
-  main()
+    main()
